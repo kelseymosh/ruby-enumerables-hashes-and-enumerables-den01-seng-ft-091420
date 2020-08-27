259ae77bbfffa4a22bd3372768be6ca4a334f6a5
Kelsey Oshiro <kelseymosh@gmail.com>
2020-08-27 18:06:42 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,9 +1,8 @@
 def select_winner(passengers)
-  winner = ""
+  winner = " "
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
+      winner
     end
   end
-
-  winner
